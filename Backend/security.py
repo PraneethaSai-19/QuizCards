43091ca823b7ce2400aa8f51e342c4f2aee673f8
PraneethaSai-19 <praneethasaimogilisetti@gmail.com>
2026-07-07 23:15:04 +0530

    Implemented JWT verification with get_current_user

--- Backend/security.py
+++ Backend/security.py
@@ -1,9 +1,42 @@
 from datetime import datetime , timedelta
 from jose import jwt , JWTError
 
+from fastapi import Depends , HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+
+from database import get_db
+from models import User
+
 SECRET_KEY = "quizcardssecretkey"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+async def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
+    credentials_exception = HTTPException(status_code=401,
+                                          detail="Could not validate credentials")
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+        email = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = db.query(User).filter(
+        User.email == email
+    ).first()
+    if user is None:
+        raise credentials_exception
+    return user
 
 def create_access_token(data: dict):
     to_encode = data.copy()
